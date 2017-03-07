79bf7df664a7b3ad36b9bb0d022aaf4b26e0c53f
lowy <lowy1160@gmail.com>
2017-03-07 17:00:56 +0800

    Update desplashWallpaper.sh

--- desplashWallpaper.sh
+++ desplashWallpaper.sh
@@ -23,4 +23,8 @@ else
 	mv $WORKDIR*'.jpg' $WORKDIR'old/'
 fi
 wget -O ${WORKDIR}${RANT}'.jpg' -q https://source.unsplash.com/1600x900
-gsettings set org.gnome.desktop.background picture-uri 'file://'${WORKDIR}${RANT}'.jpg'
+
+#gsettings set org.gnome.desktop.background picture-uri 'file://'${WORKDIR}${RANT}'.jpg'
+
+#xfce4
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s ${WORKDIR}${RANT}'.jpg'
